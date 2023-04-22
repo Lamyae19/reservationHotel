9ecefcc606665c2e324455b940700986478c69b5
MALAKSAHIR <125826533+MALAKSAHIR@users.noreply.github.com>
2023-04-22 22:39:24 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -114,6 +114,80 @@ void Room::Afficherchambre()
 
 int main()
 {
-  
-    return 0;
+	Hotel slm;		//HOTEL SLM SALMA.LAMIYA.MALAK
+	char ch;
+        
+	cout<<endl<<"=================================================================BIENVENUE AU GROUPE D'HOTELS SLM=======================================================================";
+	do           
+	{		
+		cout<<endl<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
+		cout<<endl<<"\t\t\t\t\t\t\t\t\t"
+			"MENU:\n\t\t\t\t\t\t\t\t\t"
+			"1.FONCTIONNER EN TANT QU'ADMINISTRATEUR\n\t\t\t\t\t\t\t\t\t"
+			"2.AGIR COMME CLIENT\n\t\t\t\t\t\t\t\t\t"
+			"3.SORTIE\n\t\t\t\t\t\t\t\t\t"
+			"Entrez votre choix:";
+		cin>>ch;
+		switch(ch)
+		{
+		case '1':
+			cout<<"Ajouter une base de donnees des chambres de l'hotel:"<<endl;
+			slm.addRooms();
+			cout<<"Base de donnees mise a jour. Retour au menu principal."<<endl;
+			break;
+		case '2':
+			char ch1;
+			do
+			{
+				cout<<endl<<"************************************************************************************************************************************************************************"<<endl;
+				cout<<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BIENVENUE AU GROUPE D'HOTELS SLM~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
+				cout<<"\n\t\t\t\t\t\t\t\t\t"
+					"Menu:\n\t\t\t\t\t\t\t\t\t"
+					"1.Verifier la disponibilite des chambres\n\t\t\t\t\t\t\t\t\t"
+					"2.cherches une chambre\n\t\t\t\t\t\t\t\t\t"
+					"3.Enregistrement\n\t\t\t\t\t\t\t\t\t"
+					"4.chercher un client\n\t\t\t\t\t\t\t\t\t"
+					"5.Recapitulatif des invites\n\t\t\t\t\t\t\t\t\t"
+					"6.quitter la chambre\n\t\t\t\t\t\t\t\t\t"
+					"7.Retournez au menu principal\n\t\t\t\t\t\t\t\t\t"
+					"Entrez votre choix:";
+				cin>>ch1;
+				switch(ch1)
+				{
+				case '1':
+					slm.availability();
+					break;
+				case '2':
+					slm.searchroom();
+					break;
+				case '3':
+					slm.CheckIn();
+					break;
+				case '4':
+					slm.searchcust();
+					break;
+				case '5':
+					slm.Summary();
+					break;
+				case '6':
+					slm.CheckOut();
+					break;
+				case '7':
+					break;
+				default:
+					cout<<"Choix invalide."<<endl;
+					break;
+				}
+			}while(ch1!='7');
+			break;
+		case '3':
+			cout<<"merci! :)";
+			exit(0);
+		default:
+			cout<<"Choix invalide."<<endl;
+			break;
+		}
+	}while(ch!='3');
+	return 0;
 }
+
